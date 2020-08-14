158321548ad3e278e1432e00348312755c4d61d8
vaishnavipardeshi22@gmail.com <vaishnavipardeshi22@gmail.com>
2020-08-14 22:05:23 +0530

    [Vaishnavi] ADDED : Test method to check not equal condition for inch value

--- QuantityMeasurementTest/QuantityMeasurementTest.cs
+++ QuantityMeasurementTest/QuantityMeasurementTest.cs
@@ -12,14 +12,23 @@ namespace QuantityMeasurementTest
     /// </summary>
     public class QuantityMeasurementTest
     {
+        /// <summary>
+        /// Creating object for main class.
+        /// </summary>
         private QuantityMeasurement quantityMeasurement;
 
+        /// <summary>
+        /// Set up method.
+        /// </summary>
         [SetUp]
         public void Setup()
         {
             this.quantityMeasurement = new QuantityMeasurement();
         }
 
+        /// <summary>
+        /// Test method to check equal condition for feet value.
+        /// </summary>
         [Test]
         public void GivenFeet_ShouldEqual()
         {
@@ -27,6 +36,9 @@ namespace QuantityMeasurementTest
             Assert.AreEqual(0.0, actualFeetValue);
         }
 
+        /// <summary>
+        /// Test method to check not equal condition for feet value.
+        /// </summary>
         [Test]
         public void GivenFeet_ShouldNotEqual()
         {
@@ -34,6 +46,9 @@ namespace QuantityMeasurementTest
             Assert.AreNotEqual(0.0, actualFeetValue);
         }
 
+        /// <summary>
+        /// Test method to check zero feet equals zero feet.
+        /// </summary>
         [Test]
         public void GivenTwoFeetValueAsZero_ShouldEqual()
         {
@@ -42,26 +57,38 @@ namespace QuantityMeasurementTest
             Assert.AreEqual(firstFeetValue, secondFeetValue, 0.0);
         }
 
+        /// <summary>
+        /// Test method to check zero feet is not equal to five feet.
+        /// </summary>
         [Test]
         public void GivenTwoDifferentFeet_ShouldNotEqual()
         {
             double firstFeetValue = this.quantityMeasurement.GetFeetValue(0.0);
-            double secondFeetValue = this.quantityMeasurement.GetFeetValue(1.0);
+            double secondFeetValue = this.quantityMeasurement.GetFeetValue(5.0);
             Assert.AreNotEqual(firstFeetValue, secondFeetValue);
         }
 
+        /// <summary>
+        /// Test method to check equality of null value.
+        /// </summary>
         [Test]
         public void GivenNullValue_ShouldReturnFalse()
         {
             Assert.IsFalse(this.quantityMeasurement.Equals(null));
         }
 
+        /// <summary>
+        /// Test method to check equality of reference.
+        /// </summary>
         [Test]
         public void GivenReference_shouldReturnTrue()
         {
             Assert.IsTrue(this.quantityMeasurement.Equals(this.quantityMeasurement));
         }
 
+        /// <summary>
+        /// Test method to check equality of type.
+        /// </summary>
         [Test]
         public void GivenType_shouldReturnTrue()
         {
@@ -70,6 +97,9 @@ namespace QuantityMeasurementTest
             Assert.AreEqual(firstQuantity, secondQuantity);
         }
 
+        /// <summary>
+        /// Test method to check equality of value.
+        /// </summary>
         [Test]
         public void GivenTwoSameFeet_ShouldEqual()
         {
@@ -78,11 +108,24 @@ namespace QuantityMeasurementTest
             Assert.AreEqual(firstFeetValue, secondFeetValue);
         }
 
+        /// <summary>
+        /// Test method to check equal condition for inch value.
+        /// </summary>
         [Test]
         public void GivenInch_ShouldEqual()
         {
             double actualInchValue = this.quantityMeasurement.GetInchValue(0.0);
             Assert.AreEqual(0.0, actualInchValue);
+        }
+
+        /// <summary>
+        /// Test method to check not equal condition for inch value.
+        /// </summary>
+        [Test]
+        public void GivenInch_ShouldNotEqual()
+        {
+            double actualInchValue = this.quantityMeasurement.GetInchValue(1.0);
+            Assert.AreNotEqual(0.0, actualInchValue);
         }
     }
 }
